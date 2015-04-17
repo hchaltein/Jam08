fa9ba238ebf40cc8fe3a8fa188592747cc8ac50a
unknown <ks6332@GDD-38.main.ad.rit.edu>
2015-04-17 19:22:05 -0400

    Finished layer change

---
+++ Assets/Scripts/Jam08/ChangeSpriteLayers.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeSpriteLayers : MonoBehaviour 
+{
+	void Start () 
+	{
+		gameObject.layer = gameObject.transform.parent.gameObject.layer;
+	}
+}
